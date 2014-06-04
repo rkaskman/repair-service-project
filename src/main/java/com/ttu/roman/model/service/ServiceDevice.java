d1c352817cc0b6bc969562a0917ce0d7fdbf789a
ilja <ilja.guzovski@gmail.com>
2014-06-04 10:49:52 +0300

    removed unnecessary columns

--- src/main/java/com/ttu/roman/model/service/ServiceDevice.java
+++ src/main/java/com/ttu/roman/model/service/ServiceDevice.java
@@ -21,14 +21,6 @@ public class ServiceDevice {
     private ServiceDeviceStatusType serviceDeviceStatusType;
 
     @Basic
-    @Column(name = "device_fk", nullable = true, insertable = true, updatable = true, length = 10, precision = 0)
-    private Integer deviceFk;
-
-    @Basic
-    @Column(name = "service_order_fk", nullable = true, insertable = true, updatable = true, length = 10, precision = 0)
-    private Integer serviceOrderFk;
-
-    @Basic
     @Column(name = "to_store", nullable = true, insertable = true, updatable = true, length = 29, precision = 6)
     private Timestamp toStore;
 
@@ -37,16 +29,9 @@ public class ServiceDevice {
     private Timestamp fromStore;
 
     @Basic
-    @Column(name = "service_description", nullable = true, insertable = true, updatable = true, length = 2147483647, precision = 0)
-    private String serviceDescription;
-
-    @Basic
     @Column(name = "status_changed", nullable = true, insertable = true, updatable = true, length = 29, precision = 6)
     private Timestamp statusChanged;
 
-    @Basic
-    @Column(name = "store_status", nullable = true, insertable = true, updatable = true, length = 1, precision = 0)
-    private BigInteger storeStatus;
 
     public int getServiceDevice() {
         return serviceDevice;
@@ -54,23 +39,6 @@ public class ServiceDevice {
 
     public void setServiceDevice(int serviceDevice) {
         this.serviceDevice = serviceDevice;
-    }
-
-
-    public Integer getDeviceFk() {
-        return deviceFk;
-    }
-
-    public void setDeviceFk(Integer deviceFk) {
-        this.deviceFk = deviceFk;
-    }
-
-    public Integer getServiceOrderFk() {
-        return serviceOrderFk;
-    }
-
-    public void setServiceOrderFk(Integer serviceOrderFk) {
-        this.serviceOrderFk = serviceOrderFk;
     }
 
     public Timestamp getToStore() {
@@ -89,28 +57,12 @@ public class ServiceDevice {
         this.fromStore = fromStore;
     }
 
-    public String getServiceDescription() {
-        return serviceDescription;
-    }
-
-    public void setServiceDescription(String serviceDescription) {
-        this.serviceDescription = serviceDescription;
-    }
-
     public Timestamp getStatusChanged() {
         return statusChanged;
     }
 
     public void setStatusChanged(Timestamp statusChanged) {
         this.statusChanged = statusChanged;
-    }
-
-    public BigInteger getStoreStatus() {
-        return storeStatus;
-    }
-
-    public void setStoreStatus(BigInteger storeStatus) {
-        this.storeStatus = storeStatus;
     }
 
     public ServiceDeviceStatusType getServiceDeviceStatusType() {
